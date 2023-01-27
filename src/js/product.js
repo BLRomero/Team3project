57c3038dcdd8a112ca2805d841f89309dc2de1c8
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2023-01-26 22:30:38 -0700

    updated product.js quotes

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,11 @@
-import { getParam } from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 // get the product Id
-const productId = getParam('product');
+const productId = getParam("product");
 // create instance of Product Data class
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
 // uses above
 const product = new ProductDetails(productId, dataSource);
