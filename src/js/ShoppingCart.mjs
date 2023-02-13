17b2922ce6196afd057c440f605940e41514e506
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2023-02-13 15:55:15 -0700

    add to cart function working

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function cartItemTemplate(item) {
     const newItem = `<li class="cart-card divider">
@@ -13,8 +13,8 @@ function cartItemTemplate(item) {
     </a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
     <p class="cart-card__quantity">qty: ${item.Quantity}</p>
-    <button class="qtbtn increase" value="increase">+</button>
-    <button class="qtbtn decrease">-</button>
+    <button class="qtbtn" value="${item.Id}" data-value="increase">+</button>
+    <button class="qtbtn" value="${item.Id}" data-value="decrease">-</button>
     <p class="cart-card__price">$${item.FinalPrice}</p>
   </li>`;
   
@@ -42,17 +42,23 @@ export default class ShoppingCart {
         "Your cart is empty.<br>Shop <a href='/index.html'>here</a>.";
     }
 
+    // add event listener to amount buttons
     const qtbtnsNodeList = document.querySelectorAll(".qtbtn");
     const qtbtns = Array.from(qtbtnsNodeList);
-    qtbtns.forEach(btn => btn.addEventListener("click", this.alterCart.bind(this, btn)));
+    qtbtns.forEach(btn => btn.addEventListener("click", () => {this.alterCart(cartItems, btn)}));
         }
 
-    
-    alterCart(btn) {
-      console.log(btn);
-      return
-      setLocalStorage("so-cart", this.product);
-      calcNumCartItems();
+    // adds or subtracts from cart
+    alterCart(cartItems, btn) {
+      let subtract;
+      const item = cartItems.find(function findIt(e) { if (e.Id == btn.value) return e.Id});
+      if (btn.getAttribute("data-value") == "increase") {
+        subtract = false;
+      } else {
+        subtract = true;
+      }
+      setLocalStorage("so-cart", item, subtract);
+      this.renderCartContents();
       }
   }
 
