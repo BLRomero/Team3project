e3600a250e0273f70174e8f9acb5a03e567015b0
Tim <tomlinsont@byui.edu>
2023-02-14 11:37:25 -0700

    breadcrumbs starting to display, need work on links and catagories

--- src/js/breadCrumb.js
+++ src/js/breadCrumb.js
@@ -1,15 +1,22 @@
+import { getParam } from "./utils.mjs";
+
+
 function showBreadCrumb(){
     let crumbs = window.location.pathname.split("/");
+    let prodCatagory = getParam('product');
 
     for(let index=0; index<crumbs.length-1; index++){
         let text = crumbs[index];
-        if(index==0) text = "Home";
-        document.getElementById("breadCrumbLinks").append((`<li>${text}</li>`));
-        /*document.getElementById('breadCrumbLinks').append(("<li><a class='crumb"+ (index==crumbs.length-2? "active" : "") +"' href='"+buildRelativeLink(crumbs.length-index)+"'>"+ text +"</a></li>"));*/
+        if(index==0) { 
+            text = "Home";
+        }
+       
+       document.getElementById('breadCrumbLinks').append(("<li><a class='crumb"+ (index==crumbs.length-2? "active" : "") +"' href='"+buildLink(crumbs.length-index)+"'>"+ text +"</a></li>"));
     }
+    
 }
 
-function buildRelativeLink(level)
+function buildLink(level)
 {
     level = level - 1;
     let link="";
